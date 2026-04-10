fd5a1f257a87f0a0a25555bbc93e383167d9701e
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-04-10 12:58:53 +0200

    add newsletter to show case signal forms

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HlmToaster } from '@spartan-ng/helm/sonner';
 
 @Component({
   selector: 'spartan-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmToaster],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: ` <router-outlet /> `,
+  template: `
+    <router-outlet />
+
+    @defer {
+      <hlm-toaster />
+    }
+  `,
 })
 export class App {}
